53e4cef13f475d74b453ca9a6251b1e300512e01
marco79cgn <marco79cgn@gmail.com>
2020-06-29 23:40:09 +0200

    added node sonos http api

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import soco
-
 import json
 import requests
 import time
 import os
 
-# import settings
-
 class LaSo:
-    def __init__(self, lametric_ip, lametric_user, lametric_api_key, sonos):
+    def __init__(self, lametric_ip, lametric_user, lametric_api_key, node_sonos_http_api_ip):
         self.lametric_ip = lametric_ip
         self.lametric_user = lametric_user
         self.lametric_api_key = lametric_api_key
-        self.sonos = sonos
+        self.node_sonos_http_api_ip = node_sonos_http_api_ip
 
     def get_track(self):
-        track = self.sonos.get_current_track_info()
-        status = self.sonos.get_current_transport_info()
+        node_sonos_http_api_url = "http://%s:5005/state" % (self.node_sonos_http_api_ip) 
+        response = requests.get(node_sonos_http_api_url)
+        if response.status_code == 200:
+            json_response = response.json()
+            track = {
+                "title": json_response['currentTrack']['title'],
+                "artist": json_response['currentTrack']['artist'],
+            }
+            status = json_response['playbackState']
+        else:
+            track = {
+                "title": "",
+                "artist": "",
+            } 
+            status = "STOPPED"
         return track, status
 
     def send_notification(self):
         track, status = self.get_track()
 
-        if status['current_transport_state'] == 'PLAYING':
+        if status == 'PLAYING':
 
             artist = track['artist']
             title = track['title']
@@ -30,7 +39,7 @@ class LaSo:
             api_url = "http://%s:8080/api/v2/device/notifications" % (self.lametric_ip)
             headers = {'Content-Type': 'application/json; charset=utf-8'}
             basicAuthCredentials = (self.lametric_user, self.lametric_api_key)
-            data = '{"model":{"frames":[{"icon":"19113","text":"%s - %s"}]}}' % (artist, title)
+            data = '{"priority":"warning","model":{"frames":[{"icon":"19113","text":"%s: %s"}],"cycles":3}}' % (artist, title)
             try:
                 response = requests.post(api_url,
                                          headers=headers,
@@ -49,13 +58,10 @@ class LaSo:
                 print ("Timeout Error:", errt)  
 
 def main():
-    # If you wish to show specific speaker song use its IP
-    # sonos = soco.SoCo('192.168.1.86')
-    sonos = soco.discovery.any_soco()
     laso = LaSo(os.environ["LAMETRIC_IP"],
                 "dev",
                 os.environ["LAMETRIC_API_KEY"],
-                sonos)
+                os.environ["NODE_SONOS_HTTP_API_IP"])
     # if envar DELAY isn't set than it equals 60
     delay = os.getenv('DELAY', 60)
     # print(delay)
